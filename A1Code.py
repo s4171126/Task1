fcf63d6cb35b77b7626a544dc53ea7cb91a7d6de
s4171126 <s4171126@student.rmit.edu.au>
2026-08-09 21:24:36 +1000

    Final-1
    Final code for model 1

--- A1Code.py
+++ A1Code.py
@@ -13,14 +13,14 @@ yVar=myData.iloc[:,11]
 #student_id is left out as it is not usefull here
 #final_exam_score is there is no point predicting a final grade after the final exam has been sat, there is no use in that
 
-xTrain, xTest, yTrain, yTest = train_test_split(xVars, yVar, test_size=0.2)
+xTrain, xTest, yTrain, yTest = train_test_split(xVars, yVar, test_size=0.2, random_state=1)
 splitterBest=0
 maxDepth=0
 accuracyBest=0
 splitter = ["best", "random"]
 for s in splitter:
     for i in range(1,11):
-        clf=DecisionTreeClassifier(max_depth=i,splitter=s)
+        clf=DecisionTreeClassifier(max_depth=i,splitter=s, random_state=1)
         clf2=clf.fit(xTrain, yTrain)
         predict=clf2.predict(xTest)
         accuracy=accuracy_score(yTest, predict)
@@ -30,12 +30,10 @@ for s in splitter:
             maxDepth=i
             #print(accuracy)
 
-clf=DecisionTreeClassifier(max_depth=maxDepth, splitter=splitterBest)
+clf=DecisionTreeClassifier(max_depth=maxDepth, splitter=splitterBest, random_state=1)
 finalModel=clf.fit(xTrain, yTrain)
 predict=finalModel.predict(xTest)
 finalAccuracy=accuracy_score(yTest, predict)
 
 print('Model is complete')
 print(f'Model accuracy: {finalAccuracy}')
-
-#Code is finished, need to fix random so that it is consistant
