aef1e0d7388accb41f7f47f1ea2e27674a5a4d2b
s4171126 <s4171126@student.rmit.edu.au>
2026-08-09 20:30:20 +1000

    found columns
    found columns

--- A1Code.py
+++ A1Code.py
@@ -7,4 +7,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 
 myData=pd.read_csv('student_performance_dataset.csv')
-print(myData.head())
+print (myData.columns)
+print (myData.iloc[:,1:10])
+#student_id is left out as it is not usefull here
+#final_exam_score is there is no point predicting a final grade after the final exam has been sat, there is no use in that
