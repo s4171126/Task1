9b3928c51f95e74023b51119ab9a53928c88fec4
s4171126 <s4171126@student.rmit.edu.au>
2026-08-09 19:38:19 +1000

    added data and library
    added data and library

--- A1Code.py
+++ A1Code.py
@@ -1,2 +1,10 @@
 #This is my Python Code for Case Studies in Data Science - Individual Task 1 
-#Update Test
+#This code is for a decision tree classifier
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+myData=pd.read_csv('student_performance_dataset.csv')
+print(myData.head())
